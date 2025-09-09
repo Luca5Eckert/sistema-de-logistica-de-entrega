2f6990d687fa3b82634e40cc50093264a8f47a6b
Luca5Eckert <ecklukatv@gmail.com>
2025-09-09 14:59:13 -0300

    fix: arruma classe ClienteControllerAdapter para implementar ClienteController

--- src/main/java/com/lucas/sistema/entrega/modules/cliente/application/controller/ClienteControllerAdapter.java
+++ src/main/java/com/lucas/sistema/entrega/modules/cliente/application/controller/ClienteControllerAdapter.java
@@ -3,15 +3,17 @@ package com.lucas.sistema.entrega.modules.cliente.application.controller;
 import com.lucas.sistema.entrega.modules.cliente.application.dto.ClienteAdicionarRequest;
 import com.lucas.sistema.entrega.modules.cliente.application.dto.ClienteResponse;
 import com.lucas.sistema.entrega.modules.cliente.application.port.ClienteService;
+import com.lucas.sistema.entrega.view.port.ClienteController;
 
-public class ClienteController {
+public class ClienteControllerAdapter implements ClienteController {
 
     private final ClienteService clienteService;
 
-    public ClienteController(ClienteService clienteService) {
+    public ClienteControllerAdapter(ClienteService clienteService) {
         this.clienteService = clienteService;
     }
 
+    @Override
     public ClienteResponse adicionar(ClienteAdicionarRequest clienteAdicionarRequest){
         return clienteService.adicionar(clienteAdicionarRequest);
     }
