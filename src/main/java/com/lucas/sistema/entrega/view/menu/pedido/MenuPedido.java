c7bb267059b703ef35e685caf0436e3eab29515b
Luca5Eckert <ecklukatv@gmail.com>
2025-09-16 19:34:56 -0300

    feat: adiciona do menu pedido
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

--- src/main/java/com/lucas/sistema/entrega/view/menu/pedido/MenuPedido.java
+++ src/main/java/com/lucas/sistema/entrega/view/menu/pedido/MenuPedido.java
@@ -2,6 +2,7 @@ package com.lucas.sistema.entrega.view.menu.pedido;
 
 import com.lucas.sistema.entrega.view.Leitor;
 import com.lucas.sistema.entrega.view.menu.Menu;
+import com.lucas.sistema.entrega.view.menu.MenuGeral;
 
 public class MenuPedido extends Menu {
 
@@ -14,18 +15,27 @@ public class MenuPedido extends Menu {
     public void executarMenu() {
         String input = chamarMenu();
 
+        Menu menu = switch (input.toUpperCase()){
+            case "S" -> new MenuGeral(getLeitor());
+            default -> this;
+        };
 
+        setProximoMenu(menu);
 
     }
 
     private String chamarMenu() {
         System.out.println("------------------------------------------------------------------------------");
-        System.out.println("                              MENU MOTORISTA                                  ");
+        System.out.println("                               MENU PEDIDO                                    ");
         System.out.println("------------------------------------------------------------------------------");
         System.out.println(" 1- Adicionar");
         System.out.println(" 2- Cancelar pedido");
         System.out.println(" 3- Pedidos pendentes por estado");
         System.out.println(" 4- Buscar pedido por CPF/CNPJ do cliente");
+
+        System.out.println("\n S- Sair");
+
+        return getLeitor().nextLine();
     }
 
 }
