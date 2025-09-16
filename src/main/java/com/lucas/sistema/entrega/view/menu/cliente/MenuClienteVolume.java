fad92ba2459150ea2efdc3bb17a6837c7b4304ba
Luca5Eckert <ecklukatv@gmail.com>
2025-09-15 21:12:34 -0300

    feat: adiciona menu de clientes com maior volume
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

---
+++ src/main/java/com/lucas/sistema/entrega/view/menu/cliente/MenuClienteVolume.java
@@ -0,0 +1,41 @@
+package com.lucas.sistema.entrega.view.menu.cliente;
+
+import com.lucas.sistema.entrega.modules.cliente.application.dto.ClienteResponse;
+import com.lucas.sistema.entrega.modules.entrega.application.controller.EntregaController;
+import com.lucas.sistema.entrega.view.Leitor;
+import com.lucas.sistema.entrega.view.menu.Menu;
+import com.lucas.sistema.entrega.view.port.ClienteController;
+
+import java.util.List;
+
+import static com.lucas.sistema.entrega.infraestrutura.utils.ConsoleUtil.imprimirLista;
+
+public class MenuClienteVolume extends Menu {
+
+    private final EntregaController entregaController;
+
+    public MenuClienteVolume(Leitor leitor, EntregaController entregaController) {
+        super(leitor);
+        this.entregaController = entregaController;
+    }
+
+    @Override
+    public void executarMenu() {
+        var clientes = entregaController.pegarClientesComMaiorQuantidadeEntregas();
+
+        chamarMenu(clientes);
+    }
+
+    private void chamarMenu(List<ClienteResponse> clientes) {
+
+        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("                    CLIENTES COM MAIOR VOLUME ENTREGA                         ");
+        System.out.println("------------------------------------------------------------------------------");
+
+        imprimirLista(clientes);
+
+        System.out.println("------------------------------------------------------------------------------");
+
+
+    }
+}
