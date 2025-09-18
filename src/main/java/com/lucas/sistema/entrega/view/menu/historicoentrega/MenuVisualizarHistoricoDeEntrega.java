684dbf5945320b5c8baacbf1ff5a2afa5470db91
Luca5Eckert <ecklukatv@gmail.com>
2025-09-18 19:26:23 -0300

    feat: adiciona Menu visualizar historico de entrega
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

---
+++ src/main/java/com/lucas/sistema/entrega/view/menu/historicoentrega/MenuVisualizarHistoricoDeEntrega.java
@@ -0,0 +1,59 @@
+package com.lucas.sistema.entrega.view.menu.historicoentrega;
+
+import com.lucas.sistema.entrega.modules.historicoentrega.application.dto.EventoEntregaResponse;
+import com.lucas.sistema.entrega.view.Leitor;
+import com.lucas.sistema.entrega.view.menu.Menu;
+import com.lucas.sistema.entrega.view.port.HistoricoEntregaController;
+
+import java.util.List;
+
+import static com.lucas.sistema.entrega.infraestrutura.utils.ConsoleUtil.imprimirLista;
+
+public class MenuVisualizarHistoricoDeEntrega extends Menu {
+
+    private final HistoricoEntregaController historicoEntregaController;
+
+    protected MenuVisualizarHistoricoDeEntrega(Leitor leitor, HistoricoEntregaController historicoEntregaController) {
+        super(leitor);
+        this.historicoEntregaController = historicoEntregaController;
+    }
+
+    @Override
+    public void executarMenu() {
+        var idEntrega = chamarMenu();
+
+        var historicoEntrega = historicoEntregaController.pegarHistoricoDeEntrega(idEntrega);
+
+        exibirHistorico(historicoEntrega, idEntrega);
+
+        setProximoMenu(new MenuHistoricoEntrega(getLeitor()));
+
+
+    }
+
+    private void exibirHistorico(List<EventoEntregaResponse> historicoEntrega, long idEntrega) {
+        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("                     HISTORICO DA ENTREGA " + idEntrega );
+        System.out.println("------------------------------------------------------------------------------");
+
+        imprimirLista(historicoEntrega);
+
+        System.out.println("------------------------------------------------------------------------------");
+
+    }
+
+    private long chamarMenu() {
+
+        System.out.println("------------------------------------------------------------------------------");
+        System.out.println("                           HISTORICO DE ENTREGA                               ");
+        System.out.println("------------------------------------------------------------------------------");
+        System.out.println(" Digite o id da entrega ( 0 para cancelar ) : ");
+        long idEntrega = getLeitor().nextLong();
+
+        System.out.println("------------------------------------------------------------------------------");
+
+        return idEntrega;
+
+    }
+
+}
