f44296a3ace3d622f0955412a05a39c61b278982
Luca5Eckert <ecklukatv@gmail.com>
2025-09-18 19:26:53 -0300

    feat: adiciona dto para armazenar informação historico
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

---
+++ src/main/java/com/lucas/sistema/entrega/modules/historicoentrega/application/dto/EventoEntregaResponse.java
@@ -0,0 +1,12 @@
+package com.lucas.sistema.entrega.modules.historicoentrega.application.dto;
+
+import java.time.LocalDateTime;
+
+public record EventoEntregaResponse(long id, LocalDateTime dataEvento, String descricao) {
+
+    @Override
+    public String toString() {
+        return "| Id: " + id + " | Data Evento: " + dataEvento + " | Descrição: " + descricao;
+    }
+
+}
